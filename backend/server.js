7eaf9f951d0abc15ce1d6c6e15e2428722a21e41
Majid <mhmh8760600@gmail.com>
2025-04-08 17:36:36 +0000

    Downgrade Express to 4.18.2 for compatibility

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,12 @@ app.use("/api/projects", projectRoutes);
 
 // Catch-all route to serve React's index.html (must be last)
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build", "index.html"));
+  const indexPath = path.join(__dirname, "../frontend/build", "index.html");
+  res.sendFile(indexPath, (err) => {
+    if (err) {
+      res.status(500).send("Error loading the React app.");
+    }
+  });
 });
 
 // Start the server
