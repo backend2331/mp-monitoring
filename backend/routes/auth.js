09acc1949a7144aa0c9f8fa7bb9311fdb8ae45c6
backend2331 <mhmh8760600@gmail.com>
2025-05-22 04:22:47 +0000

    Update auth.js  removed the lowercase feature

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -57,7 +57,7 @@ router.post("/login", async (req, res) => {
     if (result.rows.length === 0) {
       return res.status(401).json({ message: "Invalid username or password" });
     }
-
+    
     const user = result.rows[0];
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
